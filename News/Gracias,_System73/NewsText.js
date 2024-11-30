0455e1e4af5f7a0af3ddc40ce2aa6a5caea20c33
CurioEntrenador2 <clubingenieriaentrenador2@gmail.com>
2024-11-30 21:04:54 +0000

    Add files via upload

---
+++ News/Gracias,_System73/NewsText.js
@@ -0,0 +1,1 @@
+window.NewsTextBuffer = `La generosidad de System73 ha permitido que el Club pueda entrenar a hasta 100 alumnos y alumnas. No sabemos cómo agradecérselo con palabras, así que para ellos va esta foto.`
